3a6a88146333c2ef0c4e932636d5b4b69933b191
Alessandro Muraro <akmuraro@gmail.com>
2017-08-13 21:20:06 +0200

    adding delegate helper

---
+++ Events/delegate.js
@@ -0,0 +1,13 @@
+function delegate(parent, target, fn){
+    const parentDOM = document.querySelector(parent);
+    const targetDOM = document.querySelector(target);
+    parentDOM.addEventListener('click', (e) => {
+        e.stopPropagation();
+        if (targetDOM.isEqualNode(e.target)){
+            fn();
+        }
+    })
+}
+
+// usage:
+// delegate('.parentSelector', '.innerSelector', callbackFunction);
