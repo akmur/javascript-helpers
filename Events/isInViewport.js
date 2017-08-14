6bc3a6eb7d0f06aafb63edecc074e2f18909b8c9
Alex Muraro <akmuraro@gmail.com>
2017-08-14 11:00:38 +0200

    Update and rename is-in-viewport.js to isInViewport.js

--- Events/isInViewport.js
+++ Events/isInViewport.js
@@ -1,13 +1,13 @@
-const isInViewPort = function(elem){
-  const viewPortHeight = window.innerHeight || document.documentElement.clientHeight;
-  const viewPortWidth = window.innerWidth || document.documentElement.clientWidth;
+const isInViewport = function(elem){
+  const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+  const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
   const elementDistance = document.querySelector(elem).getBoundingClientRect();
   
   if (
     elementDistance.top >= 0 &&
     elementDistance.left >= 0 &&
-    elementDistance.bottom < viewPortHeight &&
-    elementDistance.left < viewPortWidth
+    elementDistance.bottom < viewportHeight &&
+    elementDistance.left < viewportWidth
   ) {
     return true;
   }
@@ -20,7 +20,7 @@ let timeout;
 window.addEventListener('scroll', e => {
   if (!timeout) {
     timeout = setTimeout(() => {
-      console.log(isInViewPort(selector)); // for example '#target'
+      console.log(isInViewport(selector)); // for example '#target'
       timeout = null;
     }, 66);
   }
