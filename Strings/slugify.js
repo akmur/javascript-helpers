e44232a0e07c56e3adb45db708d8461de2d7e718
Alex Muraro <akmuraro@gmail.com>
2017-08-14 14:47:52 +0200

    Update slugify.js

--- Strings/slugify.js
+++ Strings/slugify.js
@@ -1,6 +1,7 @@
 function slugify(title) {
   return title.replace(/[^\w\s]|_/g, "")
-       .toLowerCase().replace(' ', '-');
+              .toLowerCase()
+              .replace(' ', '-');
 }
 
 module.exports = slugify;
